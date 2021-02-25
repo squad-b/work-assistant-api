131195c90aa4de0cc24a1b05e052e7d4ee75ba43
오민호 <mhoh@miridih.com>
2021-02-25 21:21:44 +0900

    Fixes #2 spring project 생성

---
+++ src/main/java/com/squadb/workassistantapi/WorkAssistantApiApplication.java
@@ -0,0 +1,13 @@
+package com.squadb.workassistantapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkAssistantApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkAssistantApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/squadb/workassistantapi/WorkAssistantApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squadb.workassistantapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkAssistantApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
