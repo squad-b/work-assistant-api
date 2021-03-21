a394d68e3e175923790764ebc89cac060ac60cac
유인근 <keun0390@naver.com>
2021-03-21 23:37:10 +0900

    chore: 필요없는 import문 제거

--- src/test/java/com/squadb/workassistantapi/service/BookServiceTest.java
+++ src/test/java/com/squadb/workassistantapi/service/BookServiceTest.java
@@ -3,20 +3,15 @@ package com.squadb.workassistantapi.service;
 import com.squadb.workassistantapi.domain.Book;
 import com.squadb.workassistantapi.domain.Member;
 import com.squadb.workassistantapi.domain.MemberType;
-import org.apache.tomcat.jni.Local;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.AfterDomainEventPublication;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.persistence.EntityManager;
-import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
