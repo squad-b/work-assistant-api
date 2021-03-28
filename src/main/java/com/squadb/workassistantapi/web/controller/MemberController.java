f171ad233e4f7ccd85e741d9e64158a19b070ae6
채윤병 <yunbchae@miridih.com>
2021-03-28 21:31:00 +0900

    feat: 로그인 API 응답 수정
    - 응답에 로그인 한 멤버 id 포함
    - 응답에 로그인 실패 원인 포함

--- src/main/java/com/squadb/workassistantapi/web/controller/MemberController.java
+++ src/main/java/com/squadb/workassistantapi/web/controller/MemberController.java
@@ -3,7 +3,9 @@ package com.squadb.workassistantapi.web.controller;
 import com.squadb.workassistantapi.domain.Member;
 import com.squadb.workassistantapi.service.MemberService;
 import com.squadb.workassistantapi.web.controller.dto.LoginRequestDto;
+import com.squadb.workassistantapi.web.controller.dto.LoginResponseDto;
 import com.squadb.workassistantapi.web.controller.dto.MemberProfileResponseDto;
+import com.squadb.workassistantapi.web.exception.LoginFailedException;
 import com.squadb.workassistantapi.web.interceptor.CheckPermission;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -27,19 +29,19 @@ public class MemberController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequestDto request, HttpSession session) {
+    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto request, HttpSession session) {
         try {
-            long id = memberService.login(request.getEmail(), request.getPassword());
-            session.setAttribute("MEMBER_ID", id);
-            return ResponseEntity.ok("SUCCESS");
-        } catch (Exception e) {
-            return ResponseEntity.ok("FAIL");
+            long memberId = memberService.login(request.getEmail(), request.getPassword());
+            session.setAttribute("MEMBER_ID", memberId);
+            return ResponseEntity.ok(LoginResponseDto.success(memberId));
+        } catch (LoginFailedException e) {
+            return ResponseEntity.ok(LoginResponseDto.fail(e.getResult()));
         }
     }
 
     @CheckPermission
     @GetMapping("/members/{memberId}/profile")
-    public ResponseEntity<MemberProfileResponseDto> getMyProfile(@PathVariable long memberId) {
+    public ResponseEntity<MemberProfileResponseDto> getMemberProfile(@PathVariable long memberId) {
         try {
             final Member member = memberService.findById(memberId);
             return new ResponseEntity<>(MemberProfileResponseDto.success(member), HttpStatus.OK);
