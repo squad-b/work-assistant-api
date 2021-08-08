6032f94149ce5745f3c9c49b78f9056f1a7068a6
5minho <5minhominho@gmail.com>
2021-08-08 22:59:52 +0900

    feature #67 보안에 중요한 프로퍼티는 서버에 저장하고, 배포시에 설정 파일 경로를 주입해준다.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,10 @@ REPOSITORY_DIR=/home/ubuntu/app
 APP_NAME=work-assistant-api
 PID_PATH=$REPOSITORY_DIR/PID
 
+SESSION_PROPERTIES=$REPOSITORY_DIR/application-production-session-storage.yaml
+THIRD_PARTY_PROPERTIES=$REPOSITORY_DIR/application-thid-party.yaml
+DB_PROPERTIES=$REPOSITORY_DIR/application-production-db.yaml
+
 echo "change directory"
 
 cd $REPOSITORY_DIR/$APP_NAME
@@ -37,4 +41,7 @@ JAR_NAME=$(ls -tr $REPOSITORY_DIR/ | grep .jar | tail -n 1)
 
 echo "jar name : $REPOSITORY_DIR/$JAR_NAME"
 
-nohup java -jar $REPOSITORY_DIR/$JAR_NAME 2>&1 & echo $! > $PID_PATH
+nohup java -jar \
+  -Dspring.config.location=classpath:/application.yaml,$SESSION_PROPERTIES,$THIRD_PARTY_PROPERTIES,$DB_PROPERTIES \
+  -Dspring.profiles.active=production \
+   $REPOSITORY_DIR/$JAR_NAME 2>&1 & echo $! > $PID_PATH
