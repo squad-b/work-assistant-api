76d2aa2867f6d5ba8a4a13601bcd068e6cd05a58
YooInKeun <keun0390@naver.com>
2021-09-22 19:30:28 +0900

    test: 책 수정 기능에 대한 테스트 코드 추가

--- src/test/java/com/squadb/workassistantapi/book/domain/BookTest.java
+++ src/test/java/com/squadb/workassistantapi/book/domain/BookTest.java
@@ -1,17 +1,33 @@
 package com.squadb.workassistantapi.book.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 
+import static com.squadb.workassistantapi.book.domain.BookCategory.DEVELOP;
 import static com.squadb.workassistantapi.book.domain.IsbnTest.isbn;
 import static com.squadb.workassistantapi.member.domain.MemberTest.고객A;
 import static com.squadb.workassistantapi.member.domain.MemberTest.관리자;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BookTest {
+
+    private Book book;
+
+    @BeforeEach
+    void setUp() {
+        book = Book.builder()
+                .isbn(isbn)
+                .title("Spring")
+                .stockQuantity(StockQuantity.valueOf(1))
+                .registrationDate(LocalDateTime.now())
+                .registrant(관리자)
+                .build();
+    }
 
     @DisplayName("책의 등록자는 관리자만 가능하다")
     @Test
@@ -24,6 +40,24 @@ class BookTest {
                 .registrationDate(LocalDateTime.now())
                 .registrant(고객A)
                 .build());
+    }
+
+    @DisplayName("책의 수정은 관리자만 가능하다")
+    @Test
+    void updateBookNormalMemberTest() {
+        // when, then
+        assertThatThrownBy(() -> book.update(DEVELOP, 2, 고객A));
+    }
+
+    @DisplayName("책 수정 기능이 정상적으로 동작한다.")
+    @Test
+    void updateBookTest() {
+        // when
+        book.update(DEVELOP, 2, 관리자);
+
+        // then
+        assertEquals(book.getCategory(), DEVELOP);
+        assertEquals(book.getStockQuantityValue(), 2);
     }
 
     @DisplayName("책의 isbn, 제목, 재고, 등록일, 등록자는 필수정보이다.")
@@ -47,5 +81,4 @@ class BookTest {
         assertThat(book.getStockQuantity().getValue()).isEqualTo(1);
         assertThat(book.getRegistrationDate()).isEqualTo(registrantDateTime);
     }
-    
 }
