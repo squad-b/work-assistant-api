55d0ae1b9c77f7d083706e0146f70e29a25b2908
YooInKeun <keun0390@naver.com>
2021-10-15 22:34:39 +0900

    fix: deploy-before.sh 수정

--- deploy-before.sh
+++ deploy-before.sh
@@ -4,11 +4,3 @@ if [ -d $REPOSITORY_DIR ]; then
     sudo rm -rf $REPOSITORY_DIR
 fi
 sudo mkdir -vp $REPOSITORY_DIR
-
-cp build/libs/*.jar $REPOSITORY_DIR
-cp appspec.yml $REPOSITORY_DIR
-cp deploy-before.sh $REPOSITORY_DIR
-cp deploy.sh $REPOSITORY_DIR
-
-cd $REPOSITORY_DIR
-zip -r deploy.zip *
